d7339e4e34a66155353f04e1ee1839a06350c78d
sachin <sachinsetia1001@gmail.com>
2020-12-09 01:56:19 +0000

    Linear Data generation and transform acrooss x and y axis

---
+++ NN/perceptron_java/src/code/data/LinearSeparableData.java
@@ -0,0 +1,116 @@
+package code.data;
+
+
+import java.util.ArrayList;
+
+public class LinearSeparableData {
+    private int points;
+    private int dimension;
+    private ArrayList<Double> num_line;
+    public double range_positive;
+    public double range_negative;
+    public double c_range_positive;
+    public double c_range_negative;
+    public double closeness_to_line;
+    public double spread_from_line; // how much far point can spread from the line
+
+    public LinearSeparableData(int points, int dimension) {
+        this.dimension = dimension;
+        this.points = points;
+        this.range_negative = -10.0;
+        this.range_positive = 10.0;
+        this.c_range_negative = -4;
+        this.c_range_positive = 4;
+        this.closeness_to_line = 1.5;
+        this.spread_from_line = 10;
+    }
+
+    public ArrayList<point> getRandomData() {
+        cal_num_line();
+        ArrayList<point> data = new ArrayList<point>(this.points);
+        //TODO for now 2D only
+
+        //y = mx + c
+
+        /*
+         * Select any angle from 0 to 180, and then calculate slope
+         */
+        double f = Math.random() / Math.nextDown(1.0);
+        double theta = 90 * f;
+        double slope = Math.tan(Math.toRadians(theta));
+
+        //c get a range from c_range_negative to c_range_positive
+        double c = f * (c_range_positive - c_range_negative) + c_range_negative;
+        System.out.println("slope= " + slope + " Angle= " + theta + "intercept = " + c);
+        double range, y;
+        for (int i = 0; i < points; i++) {
+            range = Math.random() * (spread_from_line - closeness_to_line) + spread_from_line;
+            //y= mx+c (+ range for randomness)
+            y = slope * this.num_line.get(i) + c;
+            data.add(new point(num_line.get(i), y));
+//            data.add(new point(num_line.get(i), 100));
+        }
+
+        return data;
+    }
+
+    public ArrayList<point> normalize_against_axis(ArrayList<point> input, double x, double y) {
+        for (point ptr : input) {
+            ptr.x += x;
+            // Dont create this /\ pattern 
+            if (ptr.y > y)
+                ptr.y = y - ptr.y;
+            else
+                ptr.y = mode(ptr.y - y);
+        }
+        //    System.out.println(input);
+        return input;
+    }
+
+    public double mode(double num) {
+        if (num > 0)
+            return num;
+        return -num;
+    }
+
+    private ArrayList<Double> cal_num_line() {
+        double diff = (range_positive - range_negative) / points, current = range_negative;
+        ArrayList<Double> list = new ArrayList<Double>(points);
+        for (int i = 0; i < points; i++) {
+            list.add(current);
+            current += diff;
+        }
+        this.num_line = list;
+        //     System.out.println(list);
+        return num_line;
+    }
+
+
+    public static void main(String[] args) {
+        LinearSeparableData data = new LinearSeparableData(10, 0);
+        System.out.println(data.getRandomData());
+    }
+
+    public class point {
+        double x, y;
+
+        public int get_int_X() {
+            return (int) x;
+        }
+
+        public int get_int_Y() {
+            return (int) y;
+        }
+
+        point(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+
+        @Override
+        public String toString() {
+            return "X: " + x + " Y: " + y + "\n";
+        }
+    }
+}
---
+++ NN/perceptron_java/src/code/draw/DrawPanel.java
@@ -0,0 +1,89 @@
+package code.draw;
+
+import code.data.LinearSeparableData;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class DrawPanel {
+
+    private JFrame frame;
+    Panel panel;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    DrawPanel window = new DrawPanel();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the application.
+     */
+    public DrawPanel() {
+        initialize();
+    }
+
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize() {
+        frame = new JFrame();
+        frame.setBounds(100, 100, 1000, 800);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(null);
+
+        //Get the LinearSeparableData data and plot
+        LinearSeparableData data= new LinearSeparableData(1000, 2);
+        data.range_negative= -400;
+        data.range_positive= 400;
+        data.c_range_negative= -100;
+        data.c_range_positive= 100;
+        panel = new Panel(data.normalize_against_axis(data.getRandomData(), 400, 300 ), 0, 0);
+        panel.setBackground(Color.WHITE);
+        panel.setBounds(54, 35, 800, 600);
+        frame.getContentPane().add(panel);
+        panel.repaint();
+    }
+
+    private class Panel extends JPanel {
+        private ArrayList<LinearSeparableData.point> points;
+        private double slope, intercept;
+
+        Panel(ArrayList<LinearSeparableData.point> points, double slope, double intercept) {
+            super();
+            this.points = points;
+            this.slope = slope;
+            this.intercept = intercept;
+        }
+
+        public void paint(Graphics g) {
+            super.paint(g);
+            System.out.println("ss");
+            //Axis thickness
+            ((Graphics2D) g).setStroke(new BasicStroke(3));
+            //y axis
+            g.drawLine(this.getWidth() / 2, 0, this.getWidth() / 2, this.getHeight());
+            //x axis
+            g.drawLine(0, getHeight() / 2, getWidth(), this.getHeight() / 2);
+            System.out.println(this.getWidth());
+            for (LinearSeparableData.point point:points)
+            {
+                g.drawOval(point.get_int_X(),point.get_int_Y(),2,2);
+            }
+//            g.drawLine(100,200,100,200);
+        }
+    }
+}
